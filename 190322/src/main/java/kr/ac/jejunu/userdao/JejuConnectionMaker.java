6fc2347198f445a1311231126846dbf1dfd5f5ce
ssoso27 <sohi8290@naver.com>
2019-04-05 18:02:51 +0900

    [190405] DB접속정보 환경변수화

--- 190322/src/main/java/kr/ac/jejunu/userdao/JejuConnectionMaker.java
+++ 190322/src/main/java/kr/ac/jejunu/userdao/JejuConnectionMaker.java
@@ -4,7 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class JejuConnectionMaker implements ConnectionMaker {
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.password}")
+    private String password;
+    @Value("${db.username}")
+    private String username;
+
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
--- 190322/src/main/java/kr/ac/jejunu/userdao/User.java
+++ 190322/src/main/java/kr/ac/jejunu/userdao/User.java
@@ -1,31 +1,12 @@
 package kr.ac.jejunu.userdao;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
     private long id;
     private String name;
     private String password;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
