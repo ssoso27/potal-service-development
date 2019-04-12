5919992a74f6f0d3f9ff7c0443bc2328a00748e1
ssoso27 <sohi8290@naver.com>
2019-04-12 17:56:30 +0900

    [190412] 구현클래스 대신 람다로 구현

--- 190322/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ 190322/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -12,23 +12,50 @@ public class UserDao {
     }
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new GetStatementStrategy(id);
+        StatementStrategy statementStrategy = con -> {
+            PreparedStatement preparedStatement = con.prepareStatement("select * from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+
+            return preparedStatement;
+        };
+
         return jdbcContext.jdbcContextForGet(statementStrategy);
     }
 
     public long add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new AddStatementStrategy(user);
+        StatementStrategy statementStrategy = con -> {
+            PreparedStatement preparedStatement = con.prepareStatement("insert into userinfo(name, password) values (?, ?)");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+
+            return preparedStatement;
+        };
+
         return jdbcContext.jdbcContextForAdd(statementStrategy);
     }
 
     public void update(User user) throws SQLException {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        StatementStrategy statementStrategy = con -> {
+            PreparedStatement preparedStatement = con.prepareStatement("UPDATE userinfo SET name=?, password=? WHERE id=?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setLong(3, user.getId());
+
+            return preparedStatement;
+        };
+
         jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
 
     public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        StatementStrategy statementStrategy = con -> {
+            PreparedStatement preparedStatement = con.prepareStatement("DELETE FROM userinfo WHERE id = ?");
+            preparedStatement.setLong(1, id);
+
+            return preparedStatement;
+        };
+
         jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
