661b8327089dc5b31e2426d19575d381c6f20c60
ssoso27 <sohi8290@naver.com>
2019-04-12 19:07:07 +0900

    [190412] JdbcTemplate 활용

--- 190322/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ 190322/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,44 +1,72 @@
 package kr.ac.jejunu.userdao;
 
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
         String sql = "select * from userinfo where id = ?";
         Object[] params = new Object[]{id};
-        return jdbcContext.get(sql, params);
+        User result = null;
+        try {
+            result = null;
+            result = jdbcTemplate.queryForObject(sql, params, (rs, rowNum) -> {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            });
+        } catch (EmptyResultDataAccessException e) {
+        }
+        return result;
     }
 
     public long add(User user) throws ClassNotFoundException, SQLException {
         String sql = "insert into userinfo(name, password) values (?, ?)";
         Object[] params = new Object[]{user.getName(), user.getPassword()};
-        return jdbcContext.add(sql, params);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement preparedStatement
+                        = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                for (int i = 0; i < params.length; i++) {
+                    preparedStatement.setObject(i+1, params[i]);
+                }
+                return preparedStatement;
+            }
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
     }
 
     public void update(User user) throws SQLException {
         String sql = "UPDATE userinfo SET name=?, password=? WHERE id=?";
         Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
 
     public void delete(Long id) throws SQLException {
         String sql = "DELETE FROM userinfo WHERE id = ?";
         Object[] params = new Object[]{id};
-        jdbcContext.update(sql, params);
-    }
-
-    Connection getConnection() throws ClassNotFoundException, SQLException {
-
-        return jdbcContext.dataSource.getConnection();
+        jdbcTemplate.update(sql, params);
     }
 
 }
