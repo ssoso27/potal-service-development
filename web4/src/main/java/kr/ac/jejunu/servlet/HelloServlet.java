4872cf26a4d19672ed560c24c19990ea6f791909
ssoso27 <sohi8290@naver.com>
2019-05-03 18:34:45 +0900

    [190503] Servlet

---
+++ web4/src/main/java/kr/ac/jejunu/servlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package kr.ac.jejunu.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Slf4j
+public class HelloServlet extends GenericServlet {
+    @Override
+    public void init() throws ServletException {
+        log.info("*********** servlet init *************");
+        super.init();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        log.info("*********** servlet service *************");
+        res.getWriter().println("<h1> this is html tag </h1>");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("*********** servlet destroy *************");
+        super.destroy();
+    }
+}
